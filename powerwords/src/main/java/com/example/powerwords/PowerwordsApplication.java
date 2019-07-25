d6e6d7d6f85feeca1e1185e46f9d6a7d9bab1c99
NIki Funakubo <kbkbfnkb@gmail.com>
2019-07-25 15:44:16 +0900

    first commit

---
+++ powerwords/src/main/java/com/example/powerwords/PowerwordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerwordsApplication.class, args);
+	}
+
+}
